15b49656423c53a4a7afc2f22070875ef2fa2d99
Juan Cedeño <juancc0315@gmail.com>
2021-01-21 03:59:13 +0800

    CV

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,7 +20,7 @@ export const Banner = () => {
                         <p>hello</p>
                         <h3>i am juan julio cedeño </h3>
                         <h2>react and node developer</h2>
-                        <a href = 'https://drive.google.com/file/d/1BTaw8fbqByH2xCS4udEbW_96Oo7C_7ND/view?usp=sharing' target = '_blank' rel="noreferrer">Get CV</a>
+                        <a href = 'https://drive.google.com/file/d/1nj6Fpr42t32I75T9o4p7h70oi-SG0PkI/view?usp=sharing' target = '_blank' rel="noreferrer">Get CV</a>
                     </div>
 
                 </div>
